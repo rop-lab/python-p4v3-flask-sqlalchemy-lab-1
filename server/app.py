e8e1f8089d1558289f7630cfc6ea59b686593641
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-04-07 00:07:57 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, make_response
 from flask_migrate import Migrate
 from models import db, Earthquake
 
@@ -13,23 +13,26 @@ db.init_app(app)
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
+@app.route("/earthquakes/<int:id>")
+def fliter_earthquake_by_id(id):
+    
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    if earthquake:
+        body= earthquake.to_dict()
+        status=200
+    else:
+        body={"message": f"Earthquake {id} not found"}
+        status=404
+
+    return make_response(body, status)
 
 @app.route('/earthquakes/magnitude/<float:magnitude>')
 def get_earthquakes_by_magnitude(magnitude):
-    # Query the database to get earthquakes with magnitude greater than or equal to the provided value
-    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
-    quake_data = [{
-        "id": quake.id,
-        "location": quake.location,
-        "magnitude": quake.magnitude,
-        "year": quake.year
-    } for quake in earthquakes]
-
-    # Return a JSON response with count of matching earthquakes and their data
-    return jsonify({
-        "count": len(earthquakes),
-        "quakes": quake_data
-    }), 200
+    quake_data = []
+    for earthquake in db.session.query(Earthquake).filter_by(magnitude=magnitude).all():
+        quake_data.append(earthquake.to_dict())
+        body= {"count": len(quake_data), "results": quake_data}
+    return make_response (body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
